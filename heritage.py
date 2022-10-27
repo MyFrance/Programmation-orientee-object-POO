1c019b79648b3c21d7c318947840428ef328c0b5
Levasseur My France <myfrance.levasseur@gmail.com>
2022-10-27 11:37:05 +0200

    surchage de la classe

--- heritage.py
+++ heritage.py
@@ -8,23 +8,23 @@ class Utilisateur:
     def __str__(self):
        return (f"Utilisateur {self.nom}, {self.prenom}")
     
-    def Affichage(self):
-        print(f"- {self.prenom} travaille sur les projets  {projets}")
+    def Affichage_projet(self):
+       for projet in projets:
+           print(projet)
 
 # Classe héritage
 class Junior(Utilisateur):
     def __init__(self, nom, prenom):
         # Utilisation de la fonction super
         super().__init__(nom, prenom)
-
-    def restriction(self):
+# La surchange
+    def affichage_projet(self):
         for projet in projets:
             if not projet.startswith("pr_"):
                 print(f"- {self.prenom} ne travaillera que sur le projet non protégé {projet} ")
 
 
-sara = Utilisateur("Martin", "Sarah")
-sara.Affichage()
+
 marc = Junior("Marc", "Hugo")
-marc.restriction()
+marc.affichage_projet()
 
